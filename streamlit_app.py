e535a3e7489c98a9a8a69a0d38993f37a41ad509
Ronald Coe <ronald.coe@snowflake.com>
2022-11-12 09:21:02 -0500

    upload a file and create a dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,10 @@ import streamlit as st
 def introduce_app():
 	st.title("Welcome to the file uploader")
 
+def get_a_file():
+	return st.file_uploader("Locate the file to be uploaded")
+
 introduce_app()
+
+df = pd.read_csv(get_a_file())
+st.table(df)
