8cab098dcc89f4ae361b148a4aa42ac5002e6560
Ronald Coe <ronald.coe@snowflake.com>
2022-11-17 09:22:37 -0500

    Attempting connect to Snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,21 @@ import pandas as pd
 import streamlit as st
 import snowflake-snowpark-python
 
+
+
+def create_sp_session():
+  conn_param = {
+    "account": streamlit.secrets["snowflake"].account,
+    "user": streamlit.secrets["snowflake"].user,
+    "databasae": streamlit.secrets["snowflake"].database,
+    "role": streamlit.secrets["snowflake"].role,
+    "warehouse": streamlit.secrets["snowflake"].warehouse,
+    "schema": streamlit.secrets["snowflake"].schema,
+    "password": streamlit.secrets["snowflake"].password
+  }
+  session = Session.builder.configs(conn_param).create()
+  return session
+
 def introduce_app():
 	st.title("Welcome to the file uploader")
 
@@ -17,3 +32,10 @@ r_theFile = get_a_file()
 if r_theFile is not None:
 	df = pd.read_csv(r_theFile, header=None, names=['transactionDate', 'transactionAmount', 'transactionStatus'])
 	st.table(df)
+	snp_session = create_sp_session()
+	df_snp = snp_session.CreateDataFrame(df)
+	df_snp.write.mode('Overwrite').save_as_table("table_one_gb")
+
+
+
+
