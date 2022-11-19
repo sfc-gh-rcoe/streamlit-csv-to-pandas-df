459b046ab60b6397730e8c0354eb6305f502570c
Ronald Coe <ronald.coe@snowflake.com>
2022-11-18 21:27:08 -0500

    testing

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,11 +42,11 @@ def inspect_for_header(t_df, t_newNames):
 	#Inspect data frame for possible column headers
 	n_col = int(t_df.shape[1])
 	st.table(t_df)
-#	st.write(str(n_col))
-#	for j in range(n_col):
-#		# t_newNames.update({j: t_df[j]})
-#		t_newNames.update({j: t_df[j][0]})
-#	return t_newNames
+	st.write(str(n_col))
+	for j in range(n_col):
+		# t_newNames.update({j: t_df[j]})
+		t_newNames.update({j: t_df[j][0]})
+	return t_newNames
 
 def grant_header_names(t_df):
 	n_cols = df.shape[1]
