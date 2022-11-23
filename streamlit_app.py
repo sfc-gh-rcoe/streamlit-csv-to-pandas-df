6a1f10e3f89461b1b0c9cf4f27e2bfbec6e9f810
Ronald Coe <ronald.coe@snowflake.com>
2022-11-23 10:54:28 -0500

    trying the sidebar file uploader

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,7 +60,7 @@ def introduce_app():
 	st.title("Welcome to the file uploader")
 
 def get_a_file():
-	theFile = st.file_uploader("Locate the file to be uploaded")
+	theFile = st.sidebar.file_uploader("Locate the file to be uploaded")
 	if theFile is not None:
 		return theFile
 
